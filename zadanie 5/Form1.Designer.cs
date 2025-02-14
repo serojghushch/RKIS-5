57cb88e582c0431ab44190d2a1c3f47cdca8333d
serjik__1 <95130963+govnar4ick@users.noreply.github.com>
2025-02-14 13:43:04 +0300

    Добавьте файлы проекта.

---
+++ zadanie 5/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace zadanie_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.l1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(181, 159);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(570, 159);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // l1
+            // 
+            this.l1.FormattingEnabled = true;
+            this.l1.Items.AddRange(new object[] {
+            "Тангенс",
+            "Синус",
+            "Логарифм"});
+            this.l1.Location = new System.Drawing.Point(337, 118);
+            this.l1.Name = "l1";
+            this.l1.Size = new System.Drawing.Size(148, 121);
+            this.l1.TabIndex = 2;
+            this.l1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(334, 327);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Выполнил Сергей Кущян";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.l1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox l1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ zadanie 5/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zadanie_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       private void Obrabotka()
+        {
+            try
+            {
+                double x = double.Parse(textBox1.Text);
+                string select = l1.SelectedItem as string;
+                
+                if (select == null)
+                {
+                    textBox2.Text = "Выберите функцию";
+                    return;
+                }
+                double result = 0;
+                switch (select)
+                {
+                    case "Тангенс":
+                        result = Math.Tan(x);
+                        break;
+                    case "Синус":
+                        result = Math.Sin(x);
+                        break;
+                    case "Логарифм":
+                        result = Math.Log(x);
+                        break;
+                }
+                textBox2.Text = result.ToString();
+            }
+            catch (FormatException)
+            {
+                textBox2.Text = "Ошибка: Введите число";
+            }
+            catch (Exception ex)
+            {
+                textBox2.Text = "Ошибка: " + ex.Message;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Obrabotka();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Obrabotka();
+        }
+    }
+}
